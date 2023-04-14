42d85802f1305cd6bb293676808c212767080774
Nizar <nizarsaleh205@gmail.com>
2023-04-13 22:30:20 -0400

    master

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,28 +11,32 @@ public class testSelectionSort {
     public void testPositive(){
         int[] arr = new int[]{8,9,7,10,2};
         int[] sortedarr = new int[]{2,7,8,9,10};
-        
+        sort.basicSelectionSort(arr);
+        assert(Arrays.equals(arr,sortedarr));
     }
 
     @Test
     public void testNegatives(){
         int[] arr = new int[]{-8,-9,-7,-10,-2};
         int[] sortedarr = new int[]{-10,-9,-8,-7,-2};	        
-     	  
+     	  sort.basicSelectionSort(arr);
+        assert(Arrays.equals(arr,sortedarr));
 }
 
     @Test
     public void testMixed(){
         int[] arr = new int[]{-8,-9,7,-10,2,0};
         int[] sortedarr= new int[]{-10,-9,-8,0,2,7};
-        
+        sort.basicSelectionSort(arr);
+        assert(Arrays.equals(arr,sortedarr));
     }
 
     @Test
     public void testDuplicates(){
         int[] arr = new int[]{-8,-9,7,2,-8,2,-10,2,0};
         int[] sortedarr = new int[]{-10,-9,-8,-8,0,2,2,2,7};
-	   
+	  sort.basicSelectionSort(arr);
+        assert(Arrays.equals(arr,sortedarr)); 
   ;
     }
 }
